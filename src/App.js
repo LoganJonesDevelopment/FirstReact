0ae3069f1e2831fc96fcb691daf12fafda17052e
logan jones <nagol2012@yahoo.com>
2019-06-18 19:22:33 -0500

    React install and default app made

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+	// constructor method begins here:
+constructor(props) {
+  super(props);
+  this.state = { title: 'first React application!'}
+}
+	
+  render() {
+    return (
+      <h1>
+        Wow this app is my {this.state.title}
+      </h1>
+    );
+  }
+}
+
+
+export default App;
